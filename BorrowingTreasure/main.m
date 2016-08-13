612ebaf5c598debba6bd360b2c510437917f190f
辰 <wuyuancheng@wuyuanchengdeMacBook-Pro.local>
2016-08-13 09:41:00 +0800

    first commit第一次提交

---
+++ BorrowingTreasure/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BorrowingTreasure
+//
+//  Created by 辰 on 16/8/13.
+//  Copyright © 2016年 张道宏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
